c058cfd111b6c0db6ac9dee408becb0d8411cd73
Sérgio Almeida <almeida.sergiomc@gmail.com>
2025-05-05 21:52:30 +0100

    Added solution to 27. Remove Element.js

---
+++ javascript/27. Remove Element.js
@@ -0,0 +1,16 @@
+// URL: https://leetcode.com/problems/remove-element/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    // remove each num if it is equal to val
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+        }
+    }
+    return nums.length
+};
